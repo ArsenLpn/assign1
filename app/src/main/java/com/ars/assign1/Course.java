7401979fa1f612b7077d62cfcfc45cb8409e5a4b
neboskrebnut <ivanov.arseniy@gmail.com>
2019-01-18 17:26:02 -0500

    debugging works

---
+++ app/src/main/java/com/ars/assign1/Course.java
@@ -0,0 +1,29 @@
+package com.ars.assign1;
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class Course {
+    private static int courseID = 0; //static ID increments with every new Course created
+    private String courseTitle; //cou
+    private ArrayList<Assignment> assignments;
+    private Course(String title, ArrayList<Assignment> assns)
+    {
+        courseTitle = title;
+        assignments = assns;
+        courseID++;
+    }
+    //returns a Course instant with random assignment values
+    static public Course generateRandomCourse()
+    {
+        Random rnd = new Random();
+        int assignmentNo = rnd.nextInt(5);
+        ArrayList<Assignment> tempAssns = new ArrayList<Assignment>();
+        for(int i=0; i < assignmentNo; i++)
+            tempAssns.add(Assignment.generateRandomAssignment());
+        return new Course("Course " + courseID, tempAssns);
+    }
+    //****get methods*****//
+    public String getCourseTitle() {return courseTitle;}
+    public ArrayList<Assignment> getAssignments() {return assignments;}
+}
